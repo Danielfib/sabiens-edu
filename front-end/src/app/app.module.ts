2cdaf83a4996eec86e89a929bd25342ae1d8b069
Mateusnunes <mateununes@me.com>
2017-12-01 22:40:24 -0300

    resolvendo bugs e integrando alertas, alem de fazer um pagina para sistemas do Corpo

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,23 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule }   from '@angular/router';
 import { HttpModule } from '@angular/http';
-
+import { SistemaCorpo } from './sistemasCorpo.component';
 import { CadastroConteudo } from './cadastroConteudo.component';
 import { ConteudoService } from './conteudo.service';
 import { AppComponent } from './app.component';
-
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CadastroConteudo,
+    SistemaCorpo,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule, 
     RouterModule.forRoot([
+      {
+        path: 'sistemas',
+        component: SistemaCorpo
+      },
       {
         path: 'cadastroConteudo',
         component: CadastroConteudo
